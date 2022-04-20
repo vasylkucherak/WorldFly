97002ad55c64f38c9b3e37e15ec6f25eb413af79
vasylkucherak <vasil.kucherak@gmail.com>
2022-04-20 16:57:34 +0300

    v.0.0.2(added parallax effect on the site)

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,69 @@
 'use strict';
+
+const promo = document.querySelector('.promo');
+
+if (promo) {
+    const content = document.querySelector('.promo__content');
+    const bg = document.querySelector('.img-parallax__bg');
+    const cloud3 = document.querySelector('.img-parallax__cloud3');
+    const cloud2 = document.querySelector('.img-parallax__cloud2');
+    const cloud1 = document.querySelector('.img-parallax__cloud1');
+    const back2 = document.querySelector('.img-parallax__back2');
+    const back1 = document.querySelector('.img-parallax__back1');
+    const lake = document.querySelector('.img-parallax__lake');
+    const middle3 = document.querySelector('.img-parallax__middle3');
+    const middle2 = document.querySelector('.img-parallax__middle2');
+    const middle1 = document.querySelector('.img-parallax__middle1');
+    const front = document.querySelector('.img-parallax__front');
+
+    const forBg = 50;
+    const forCloud3 = 45;
+    const forCloud2 = 40;
+    const forCloud1 = 35;
+    const forBack2 = 30;
+    const forBack1 = 25;
+    const forLake = 21;
+    const forMiddle3 = 21;
+    const forMiddle2 = 18;
+    const forMiddle1 = 15;
+    const forFront = 11;
+
+    const speed = 0.05;
+
+    let positionX = 0, positionY = 0;
+    let coordXprocent = 0, coordYprocent = 0;
+
+    function setMouseParallax() {
+        const distX = coordXprocent - positionX;
+        const distY = coordYprocent - positionY;
+
+        positionX = positionX + (distX * speed);
+        positionY = positionY + (distY * speed);
+
+        bg.style.cssText = `transform: translate(${positionX / forBg}%,${positionY / forBg}%);`;
+        cloud3.style.cssText = `transform: translate(${positionX / forCloud3}%,${positionY / forCloud3}%);`;
+        cloud2.style.cssText = `transform: translate(${positionX / forCloud2}%,${positionY / forCloud2}%);`;
+        cloud1.style.cssText = `transform: translate(${positionX / forCloud1}%,${positionY / forCloud1}%);`;
+        back2.style.cssText = `transform: translate(${positionX / forBack2}%,${positionY / forBack2}%);`;
+        back1.style.cssText = `transform: translate(${positionX / forBack1}%,${positionY / forBack1}%);`;
+        lake.style.cssText = `transform: translate(${positionX / forLake}%,${positionY / forLake}%);`;
+        middle3.style.cssText = `transform: translate(${positionX / forMiddle3}%,${positionY / forMiddle3}%);`;
+        middle2.style.cssText = `transform: translate(${positionX / forMiddle2}%,${positionY / forMiddle2}%);`;
+        middle1.style.cssText = `transform: translate(${positionX / forMiddle1}%,${positionY / forMiddle1}%);`;
+        front.style.cssText = `transform: translate(${positionX / forFront}%,${positionY / forFront}%);`;
+
+        requestAnimationFrame(setMouseParallax);
+    }
+    setMouseParallax();
+
+    promo.addEventListener('mousemove', function(e) {
+        const promoWidth = promo.offsetWidth;
+        const promoHeight = promo.offsetHeight;
+
+        const coordX = e.pageX - promoWidth / 2;
+        const coordY = e.pageY - promoHeight / 2;
+
+        coordXprocent = coordX / promoWidth * 100;
+        coordYprocent = coordY / promoHeight * 100;
+    });
+}
--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
-    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
+    gulp.watch("src/script.js").on('change', gulp.parallel('scripts'));
     gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
     gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
     gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
@@ -44,7 +44,7 @@ gulp.task('html', function () {
 });
 
 gulp.task('scripts', function () {
-    return gulp.src("src/**/*.js")
+    return gulp.src("src/script.js")
         .pipe(gulp.dest("dist/"))
         .pipe(browserSync.stream());
 });
