9677000556d882db578c89a9d583d99daca7804d
vasylkucherak <vasil.kucherak@gmail.com>
2022-04-24 15:06:37 +0300

    v.0.0.9(writed script for slider in popular section)

--- src/script.js
+++ src/script.js
@@ -96,4 +96,45 @@ window.onload = function() {
             front.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%);`;
         }
     }
+
+    //============ First Slider =====================================================================================
+    const sliderList = document.querySelector('.popular__slider-list'),
+          sliderCards = sliderList.querySelectorAll('.popular__slider-card'),
+          sliderBtnPrev = document.querySelector('.popular__slider-prev'),
+          sliderBtnNext = document.querySelector('.popular__slider-next');
+    
+    let scrollCounter = 0;
+            
+    sliderBtnNext.classList.add('active');
+    sliderBtnNext.addEventListener('click', () => {
+        if (scrollCounter < sliderCards.length - 1) {
+            scrollCounter++;
+            sliderList.style.transform = `translateX(-${scrollCounter * 60}vw)`;
+            sliderBtnNext.classList.add('active');
+            sliderBtnPrev.classList.add('active');
+        }
+        if (scrollCounter == sliderCards.length - 1) {
+            sliderBtnNext.classList.remove('active');
+        }
+        sliderCards.forEach(sliderCard => {
+            sliderCard.classList.remove('active');
+        });
+        sliderCards[scrollCounter].classList.add('active');
+
+    });
+    sliderBtnPrev.addEventListener('click', () => {
+        if (scrollCounter > 0) {
+            scrollCounter--;
+            sliderList.style.transform = `translateX(-${scrollCounter * 60}vw)`;
+            sliderBtnPrev.classList.add('active');
+            sliderBtnNext.classList.add('active');
+        }
+        if (scrollCounter == 0) {
+            sliderBtnPrev.classList.remove('active');
+        }
+        sliderCards.forEach(sliderCard => {
+            sliderCard.classList.remove('active');
+        });
+        sliderCards[scrollCounter].classList.add('active');
+    });
 }
